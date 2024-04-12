3b41a7d1a9802f61a820375c25d7f5acc952c48c
Michael Gauthier <mikegauthier222@hotmail.com>
2024-04-12 12:51:25 -0700

    add functionality to view and add categories as admin

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,11 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, as: 'categories'
   end
+
+
+  
 
   get '/about', to: 'about#index'
 
