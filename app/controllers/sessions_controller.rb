34a49b5b8455bb1189aa5404c42764d9ad3e6044
Michael Gauthier <mikegauthier222@hotmail.com>
2024-04-14 11:23:13 -0700

    fix login error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,10 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    # If the user exists AND the password entered is correct.
     if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
-      # logged in when they navigate around our website.
       session[:user_id] = user.id
       redirect_to root_path, notice: 'Logged in successfully!'
     else
-    # If user's login doesn't work, send them back to the login form.
     flash.now[:alert] = 'Invalid email or password'
     render :new
     end
